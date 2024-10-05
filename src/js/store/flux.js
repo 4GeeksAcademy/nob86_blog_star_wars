5d4f23628e0a421ebd237fc9c4f4e16982f976fb
nob86 <gustavoadallacroce@gmail.com>
2024-10-05 15:07:13 +0000

    films

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,193 +1,173 @@
-// const getState = ({ getStore, getActions, setStore }) => {
-// 	return {
-// 		store: {
-// 			demo: [
-// 				{
-// 					title: "FIRST",
-// 					background: "white",
-// 					initial: "white"
-// 				},
-// 				{
-// 					title: "SECOND",
-// 					background: "white",
-// 					initial: "white"
-// 				}
-// 			],
-// 			favoritos : [],
-// 			planets : [],
-// 			singlePlanet : null,
-// 			people : [],
-
-// 		},
-// 		actions: {
-// 			//fetchPlanetData: 
-// 			//funcion que hara fethc get de planetas y luego se ta hara la variable planets con la informacion del fetch 
-// 			// Use getActions to call a function within a fuction
-// 			exampleFunction: () => {
-// 				getActions().changeColor(0, "green");
-// 			},
-// 			loadSomeData: () => {
-// 				/**
-// 					fetch().then().then(data => setStore({ "foo": data.bar }))
-// 				*/
-// 			},
-// 			changeColor: (index, color) => {
-// 				//get the store
-// 				const store = getStore();
-
-// 				//we have to loop the entire demo array to look for the respective index
-// 				//and change its color
-// 				const demo = store.demo.map((elm, i) => {
-// 					if (i === index) elm.background = color;
-// 					return elm;
-// 				});
-
-// 				//reset the global store
-// 				setStore({ demo: demo });
-// 			}
-// 		}
-// 	};
-// };
-
-// export default getState;
-
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			favoritos: [],
-			planets: [],  // Aquí se almacenarán los planetas
-			singlePlanet: null,  // Para almacenar un solo planeta si es necesario
-			people: [],
-			singlePeople : null,
-			starships: [],
-			singleStarship: null
-		},
-		actions: {
-			// Función para hacer fetch de los planetas
-			fetchPlanetsData: () => {
-				fetch("https://www.swapi.tech/api/planets")
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener los planetas");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos la lista de planetas en el store
-						setStore({ planets: data.results });
-					})
-					.catch(error => {
-						console.error("Error fetching planets: ", error);
-					});
-			},
+  return {
+    store: {
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      favoritos: [],
+      planets: [], // Aquí se almacenarán los planetas
+      singlePlanet: null, // Para almacenar un solo planeta si es necesario
+      people: [],
+      singlePeople: null,
+      starships: [],
+      singleStarship: null,
+      films: [],
+      singleFilm: null,
+    },
+    actions: {
+      // Función para hacer fetch de los planetas
+      fetchPlanetsData: () => {
+        fetch("https://www.swapi.tech/api/planets")
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener los planetas");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos la lista de planetas en el store
+            setStore({ planets: data.results });
+          })
+          .catch((error) => {
+            console.error("Error fetching planets: ", error);
+          });
+      },
 
-			// Función para obtener un solo planeta por ID
-			fetchSinglePlanet: (id) => {
-				fetch(`https://www.swapi.tech/api/planets/${id}`)
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener el planeta");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos el planeta individual en el store
-						setStore({ singlePlanet: data.result });
-					})
-					.catch(error => {
-						console.error("Error fetching single planet: ", error);
-					});
-			},
+      // Función para obtener un solo planeta por ID
+      fetchSinglePlanet: (id) => {
+        fetch(`https://www.swapi.tech/api/planets/${id}`)
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener el planeta");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos el planeta individual en el store
+            setStore({ singlePlanet: data.result });
+          })
+          .catch((error) => {
+            console.error("Error fetching single planet: ", error);
+          });
+      },
 
-			fetchPeopleData: () => {
-				fetch("https://www.swapi.tech/api/people")
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener los people");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos la lista de planetas en el store
-						setStore({ people: data.results });
-					})
-					.catch(error => {
-						console.error("Error fetching planets: ", error);
-					});
-			},
+      fetchPeopleData: () => {
+        fetch("https://www.swapi.tech/api/people")
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener los people");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos la lista de planetas en el store
+            setStore({ people: data.results });
+          })
+          .catch((error) => {
+            console.error("Error fetching planets: ", error);
+          });
+      },
 
-			// Función para obtener un solo planeta por ID
-			fetchSinglePeople: (id) => {
-				fetch(`https://www.swapi.tech/api/people/${id}`)
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener people");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos el planeta individual en el store
-						setStore({ singlePeople: data.result });
-					})
-					.catch(error => {
-						console.error("Error fetching single people: ", error);
-					});
-			},
+      // Función para obtener un solo planeta por ID
+      fetchSinglePeople: (id) => {
+        fetch(`https://www.swapi.tech/api/people/${id}`)
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener people");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos el planeta individual en el store
+            setStore({ singlePeople: data.result });
+          })
+          .catch((error) => {
+            console.error("Error fetching single people: ", error);
+          });
+      },
 
-			fetchStarshipsData: () => {
-				fetch("https://www.swapi.tech/api/starships")
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener los starthips");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos la lista de planetas en el store
-						setStore({ starships: data.results });
-					})
-					.catch(error => {
-						console.error("Error fetching starships: ", error);
-					});
-			},
+      fetchStarshipsData: () => {
+        fetch("https://www.swapi.tech/api/starships")
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener los starthips");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos la lista de planetas en el store
+            setStore({ starships: data.results });
+          })
+          .catch((error) => {
+            console.error("Error fetching starships: ", error);
+          });
+      },
 
-			// Función para obtener un solo planeta por ID
-			fetchSingleStarship: (id) => {
-				fetch(`https://www.swapi.tech/api/starships/${id}`)
-					.then(response => {
-						if (!response.ok) throw new Error("Error al obtener el starship");
-						return response.json();
-					})
-					.then(data => {
-						// Aquí almacenamos el planeta individual en el store
-						setStore({ singleStarship: data.result });
-					})
-					.catch(error => {
-						console.error("Error fetching single starship: ", error);
-					});
-			},
+      // Función para obtener un solo planeta por ID
+      fetchSingleStarship: (id) => {
+        fetch(`https://www.swapi.tech/api/starships/${id}`)
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener el starship");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos el planeta individual en el store
+            setStore({ singleStarship: data.result });
+          })
+          .catch((error) => {
+            console.error("Error fetching single starship: ", error);
+          });
+      },
 
+      fetchFilmsData: () => {
+        fetch("https://www.swapi.tech/api/films")
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener los films");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos la lista de planetas en el store
+            const films = data.result.map((film) => film.properties);
+            setStore({ films: films });
+          })
+          .catch((error) => {
+            console.error("Error fetching films: ", error);
+          });
+      },
 
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+      // Función para obtener un solo planeta por ID
+      fetchSingleFilm: (id) => {
+        fetch(`https://www.swapi.tech/api/films/${id}`)
+          .then((response) => {
+            if (!response.ok) throw new Error("Error al obtener el film");
+            return response.json();
+          })
+          .then((data) => {
+            // Aquí almacenamos el planeta individual en el store
+            setStore({ singleFilm: data.result });
+          })
+          .catch((error) => {
+            console.error("Error fetching single film: ", error);
+          });
+      },
 
-			loadSomeData: () => {
-				// Puedes hacer otros fetch aquí si es necesario
-			},
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			changeColor: (index, color) => {
-				const store = getStore();
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-				setStore({ demo: demo });
-			}
-		}
-	};
+      loadSomeData: () => {
+        // Puedes hacer otros fetch aquí si es necesario
+      },
+
+      changeColor: (index, color) => {
+        const store = getStore();
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,68 +1,66 @@
-// import React from "react";
-// import rigoImage from "../../img/rigo-baby.jpg";
-// import "../../styles/home.css";
-
-// export const Home = () => (
-// 	<div className="text-center mt-5">
-// 		<h1>Hello Rigo!</h1>
-// 		<p>
-// 			<img src={rigoImage} />
-// 		</p>
-// 		<a href="#" className="btn btn-success">
-// 			If you see this green button, bootstrap is working
-// 		</a>
-// 	</div>
-// );
-
 import React, { useEffect, useContext } from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Context } from "../store/appContext"; // Importa el contexto para acceder al store y las acciones
 
 export const Home = () => {
-	// Acceder al store y actions desde el contexto
-	const { store, actions } = useContext(Context);
+  // Acceder al store y actions desde el contexto
+  const { store, actions } = useContext(Context);
 
-	// Llamar a la función que obtiene los planetas cuando el componente se monta
-	useEffect(() => {
-		actions.fetchPlanetsData();
-		actions.fetchPeopleData();
-		actions.fetchStarshipsData();
-	}, []);
+  // Llamar a la función que obtiene los planetas cuando el componente se monta
+  useEffect(() => {
+    actions.fetchPlanetsData();
+    actions.fetchPeopleData();
+    actions.fetchStarshipsData();
+    actions.fetchFilmsData();
+  }, []);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Lista de Planetas</h1>
-			{/* Mostrar la lista de planetas desde el store */}
-			<ul>
-				{store.planets.length > 0 ? (
-					store.planets.map((planet, index) => (
-						<li key={index}>{planet.name}</li> // Renderizar cada planeta
-					))
-				) : (
-					<li>Cargando planetas...</li> // Mensaje mientras se cargan los planetas
-				)}
-			</ul>
-			<h1>Lista de People</h1>
-			<ul>
-				{store.people.length > 0 ? (
-					store.people.map((people, index) => (
-						<li key={index}>{people.name}</li> // Renderizar cada planeta
-					))
-				) : (
-					<li>Cargando people...</li> // Mensaje mientras se cargan los planetas
-				)}
-			</ul>
-			<h1>Lista de starships</h1>
-			<ul>
-				{store.starships.length > 0 ? (
-					store.starships.map((starship, index) => (
-						<li key={index}>{starship.name}</li> // Renderizar cada planeta
-					))
-				) : (
-					<li>Cargando starships...</li> // Mensaje mientras se cargan los planetas
-				)}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>Lista de Films</h1>
+      {/* Mostrar la lista de planetas desde el store */}
+      <ul>
+        {store.films?.length > 0 ? (
+          store.films.map((film, index) => (
+            <li key={index}>
+              {film.title} - Dirigida por: {film.director}
+            </li> // Renderizar cada planeta
+          ))
+        ) : (
+          <li>Cargando films...</li> // Mensaje mientras se cargan los planetas
+        )}
+      </ul>
+
+      <h1>Lista de Planetas</h1>
+      {/* Mostrar la lista de planetas desde el store */}
+      <ul>
+        {store.planets.length > 0 ? (
+          store.planets.map((planet, index) => (
+            <li key={index}>{planet.name}</li> // Renderizar cada planeta
+          ))
+        ) : (
+          <li>Cargando planetas...</li> // Mensaje mientras se cargan los planetas
+        )}
+      </ul>
+      <h1>Lista de People</h1>
+      <ul>
+        {store.people.length > 0 ? (
+          store.people.map((people, index) => (
+            <li key={index}>{people.name}</li> // Renderizar cada planeta
+          ))
+        ) : (
+          <li>Cargando people...</li> // Mensaje mientras se cargan los planetas
+        )}
+      </ul>
+      <h1>Lista de starships</h1>
+      <ul>
+        {store.starships.length > 0 ? (
+          store.starships.map((starship, index) => (
+            <li key={index}>{starship.name}</li> // Renderizar cada planeta
+          ))
+        ) : (
+          <li>Cargando starships...</li> // Mensaje mientras se cargan los planetas
+        )}
+      </ul>
+    </div>
+  );
 };
