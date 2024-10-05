f8fae536897e16675cec05f223cd5585984c06db
nob86 <gustavoadallacroce@gmail.com>
2024-10-05 00:29:40 +0000

    empezando

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,56 @@
+// const getState = ({ getStore, getActions, setStore }) => {
+// 	return {
+// 		store: {
+// 			demo: [
+// 				{
+// 					title: "FIRST",
+// 					background: "white",
+// 					initial: "white"
+// 				},
+// 				{
+// 					title: "SECOND",
+// 					background: "white",
+// 					initial: "white"
+// 				}
+// 			],
+// 			favoritos : [],
+// 			planets : [],
+// 			singlePlanet : null,
+// 			people : [],
+
+// 		},
+// 		actions: {
+// 			//fetchPlanetData: 
+// 			//funcion que hara fethc get de planetas y luego se ta hara la variable planets con la informacion del fetch 
+// 			// Use getActions to call a function within a fuction
+// 			exampleFunction: () => {
+// 				getActions().changeColor(0, "green");
+// 			},
+// 			loadSomeData: () => {
+// 				/**
+// 					fetch().then().then(data => setStore({ "foo": data.bar }))
+// 				*/
+// 			},
+// 			changeColor: (index, color) => {
+// 				//get the store
+// 				const store = getStore();
+
+// 				//we have to loop the entire demo array to look for the respective index
+// 				//and change its color
+// 				const demo = store.demo.map((elm, i) => {
+// 					if (i === index) elm.background = color;
+// 					return elm;
+// 				});
+
+// 				//reset the global store
+// 				setStore({ demo: demo });
+// 			}
+// 		}
+// 	};
+// };
+
+// export default getState;
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -12,30 +65,59 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			favoritos: [],
+			planets: [],  // Aquí se almacenarán los planetas
+			singlePlanet: null,  // Para almacenar un solo planeta si es necesario
+			people: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+			// Función para hacer fetch de los planetas
+			fetchPlanetsData: () => {
+				fetch("https://www.swapi.tech/api/planets")
+					.then(response => {
+						if (!response.ok) throw new Error("Error al obtener los planetas");
+						return response.json();
+					})
+					.then(data => {
+						// Aquí almacenamos la lista de planetas en el store
+						setStore({ planets: data.results });
+					})
+					.catch(error => {
+						console.error("Error fetching planets: ", error);
+					});
+			},
+
+			// Función para obtener un solo planeta por ID
+			fetchSinglePlanet: (id) => {
+				fetch(`https://www.swapi.tech/api/planets/${id}`)
+					.then(response => {
+						if (!response.ok) throw new Error("Error al obtener el planeta");
+						return response.json();
+					})
+					.then(data => {
+						// Aquí almacenamos el planeta individual en el store
+						setStore({ singlePlanet: data.result });
+					})
+					.catch(error => {
+						console.error("Error fetching single planet: ", error);
+					});
+			},
+
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
+			loadSomeData: () => {
+				// Puedes hacer otros fetch aquí si es necesario
+			},
+
+			changeColor: (index, color) => {
+				const store = getStore();
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
-				//reset the global store
 				setStore({ demo: demo });
 			}
 		}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,46 @@
-import React from "react";
+// import React from "react";
+// import rigoImage from "../../img/rigo-baby.jpg";
+// import "../../styles/home.css";
+
+// export const Home = () => (
+// 	<div className="text-center mt-5">
+// 		<h1>Hello Rigo!</h1>
+// 		<p>
+// 			<img src={rigoImage} />
+// 		</p>
+// 		<a href="#" className="btn btn-success">
+// 			If you see this green button, bootstrap is working
+// 		</a>
+// 	</div>
+// );
+
+import React, { useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext"; // Importa el contexto para acceder al store y las acciones
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	// Acceder al store y actions desde el contexto
+	const { store, actions } = useContext(Context);
+
+	// Llamar a la función que obtiene los planetas cuando el componente se monta
+	useEffect(() => {
+		actions.fetchPlanetsData(); // Llama al fetch de planetas
+	}, []);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Lista de Planetas</h1>
+			{/* Mostrar la lista de planetas desde el store */}
+			<ul>
+				{store.planets.length > 0 ? (
+					store.planets.map((planet, index) => (
+						<li key={index}>{planet.name}</li> // Renderizar cada planeta
+					))
+				) : (
+					<li>Cargando planetas...</li> // Mensaje mientras se cargan los planetas
+				)}
+			</ul>
+		</div>
+	);
+};
