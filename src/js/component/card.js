858c91e581fa5ba7fd529989a31bdbf858ed4f86
nob86 <gustavoadallacroce@gmail.com>
2024-10-09 13:50:55 +0000

    sin copiar

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -7,69 +7,8 @@ const Card = ({ name, type, id }) => {
         <img
           src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`}
           className="card-img-top"
-          alt="..."
+          alt={name}
         />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{ width: "18rem;" }}>
-        <img
-          src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`}
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{ width: "18rem;" }}>
-        <img
-          src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`}
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{ width: "18rem;" }}>
-        <img
-          src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`}
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{ width: "18rem;" }}>
-        <img
-          src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`}
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{width: "18rem;"}}>
-        <img src={`https://starwars-visualguide.com/assets/img/${type}/${id}.jpg`} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <a href="#" className="btn btn-primary">
